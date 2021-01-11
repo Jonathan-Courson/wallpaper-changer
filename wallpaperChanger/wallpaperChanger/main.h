6cab6e19a833bebc3b904115302dc5881b8dedd6
Jonathan Courson <jonathan_courson@insideranken.org>
2021-01-10 20:02:28 -0600

    a

--- wallpaperChanger/wallpaperChanger/main.h
+++
@@ -1,8 +0,0 @@
-#define main
-#include "hidsdi.h"
-
-int getRandom(int count);
-void addFilesToList(fs::path paths);
-void setBackground(fs::path path);
-void updatePaths(vector<string> newPaths);
-void recalculateImageList();
